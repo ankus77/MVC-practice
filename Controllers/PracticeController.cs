83c38a2769143985bddb4922afa3e132e0aa34c9
ankusnayak7 <ankusnayak7@gmail.com>
2020-01-18 02:41:01 +0530

    create new PracticeController and Test1 model and GetEmployeeById , GetEmployee views

---
+++ Controllers/PracticeController.cs
@@ -0,0 +1,39 @@
+﻿using FirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class PracticeController : Controller
+    {
+        // GET: Practice
+        public ActionResult IndexPractice()
+        {
+            return View();
+        }
+        public ActionResult GetEmployee()
+        {
+            var empList = new List<Test1>();
+            empList.Add(new Test1() { EmpId = 1, EmpName = "ankus nayak", EmpPhoneNumber="100",EmpProjectId=1 });
+            empList.Add(new Test1() { EmpId = 2, EmpName = "ankan nayak", EmpPhoneNumber = "101", EmpProjectId = 2 });
+            empList.Add(new Test1() { EmpId = 3, EmpName = "sumita nayak", EmpPhoneNumber = "102", EmpProjectId = 1 });
+            empList.Add(new Test1() { EmpId = 4, EmpName = "arun nayak", EmpPhoneNumber = "103", EmpProjectId = 2 });
+            return View(empList);
+        }
+        public ActionResult GetEmployeeById(int n)
+        {
+            var empObject = new Test1();
+            var empList = new List<Test1>();
+            empList.Add(new Test1() { EmpId = 1, EmpName = "ankus nayak", EmpPhoneNumber = "100", EmpProjectId = 1 });
+            empList.Add(new Test1() { EmpId = 2, EmpName = "ankan nayak", EmpPhoneNumber = "101", EmpProjectId = 2 });
+            empList.Add(new Test1() { EmpId = 3, EmpName = "sumita nayak", EmpPhoneNumber = "102", EmpProjectId = 1 });
+            empList.Add(new Test1() { EmpId = 4, EmpName = "arun nayak", EmpPhoneNumber = "103", EmpProjectId = 2 });
+            empObject = empList.Find(x => x.EmpId == n);
+            return View(empObject);
+
+        }
+    }
+}
---
+++ Models/Test1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApp.Models
+{
+    //create a data class
+    public class Test1
+    {
+        [Display(Name="Employee Id")]
+         public int EmpId { get; set; }
+        [Display(Name ="Employee Name")]
+        public string EmpName { get; set; }
+        [Display(Name ="Mobile Number")]
+        public string EmpPhoneNumber { get; set; }
+        [Display(Name ="Project Id")]
+        public int EmpProjectId { get; set; }
+
+
+    }
+}
